cc4f376763eb3c4e65ceca27f1c12747113beeb2
rmobis <r.mobis@gmail.com>
2015-01-27 22:19:33 -0200

    Reorganiza assets and do some cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -14,31 +14,8 @@ require('laravel-elixir-jshint');
 
 elixir(function(mixx) {
 	mixx
-		.sass('raphseller.scss')
-    	/* JavaScript */
-		.copy(
-			'vendor/bower_components/jquery/dist/jquery.min.js',
-			'public/js/vendor/jquery.js'
-		)
-		.copy(
-			'vendor/bower_components/jquery-throttle-debounce/jquery.ba-throttle-debounce.js',
-			'public/js/vendor/jquery.ba-throttle-debounce.js'
-		)
-		.copy(
-			'vendor/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert.js',
-			'public/js/vendor/bootstrap-alert.js'
-		)
-		.jshint([
-			'public/js/**/*.js',
-    		'!public/js/vendor/**/*.js',
-    		'!**/raphseller.js'
-    	])
-		.scripts([
-			'js/vendor/bootstrap-alert.js',
-			'js/vendor/jquery.ba-throttle-debounce.js'
-		], null, 'public/js/raphseller.js')
-
 		/* SASS/CSS */
+		.sass('raphseller.scss')
 		.copy(
 			'vendor/bower_components/bootstrap-sass-official/assets/fonts/bootstrap',
 			'public/css/fonts'
@@ -47,11 +24,42 @@ elixir(function(mixx) {
 			'vendor/bower_components/bootstrap-sass-official/assets/stylesheets',
 			'resources/assets/sass/vendor'
 		)
+		
+		/* JavaScript */
+		.jshint([
+			'resources/assets/js/**/*.js'
+    	])
+		.scripts([
+			'js/vendor/bootstrap-alert.js',
+			'js/vendor/jquery.ba-throttle-debounce.js'
+		], null, 'public/js/raphseller.js')
+		.copy(
+			'vendor/bower_components/jquery/dist/jquery.min.js',
+			'public/js/vendor/jquery.js'
+		)
+		.copy(
+			'vendor/bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/bootstrap-alert.js',
+			'public/js/vendor/bootstrap-alert.js'
+		)
+		.copy(
+			'vendor/bower_components/jquery-throttle-debounce/jquery.ba-throttle-debounce.js',
+			'public/js/vendor/jquery.ba-throttle-debounce.js'
+		)
+		.copy(
+			'resources/assets/js',
+			'public/js'
+		)
+
+		/* Images */
+		.copy(
+			'resources/assets/img',
+			'public/img'
+		)
 
 		/* Cache Busting */
 		.version([
 			'public/css/raphseller.css',
 			'public/js/raphseller.js',
-			'public/js/pages/order.create.js'
+			'public/js/pages/order.create.js',
 		]);
 });
