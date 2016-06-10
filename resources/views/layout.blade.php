819df8ebbac6d24b8e8158492a9d4ed06b1bd0f3
Raphael Mobis Tacla <r.mobis@gmail.com>
2016-06-10 15:57:11 -0300

    Install hotjar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,18 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+
+		<!-- Hotjar Tracking Code for http://raphseller.com -->
+		<script>
+			(function(h,o,t,j,a,r){
+				h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+				h._hjSettings={hjid:229601,hjsv:5};
+				a=o.getElementsByTagName('head')[0];
+				r=o.createElement('script');r.async=1;
+				r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+				a.appendChild(r);
+			})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
 	</head>
 
 	<body>
